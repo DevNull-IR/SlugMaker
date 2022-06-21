71b15b9ca50b72d578cd3cc92dd8ecd42adc7180
Ali <97373457+DevNull-IR@users.noreply.github.com>
2022-06-21 10:41:03 +0430

    Update helpers.php

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -7,12 +7,12 @@ if (! function_exists('slugMaker')){
 		$is_lang = $data['is_lang'] ?? false;
 		$slug = str_replace('?',null,trim($slug));
 		$slug = str_replace('!',null,trim($slug));
-		$slug = str_replace(['#','$','%','^','&','*','(',')','.','=','+'],null,trim($slug));
+		$slug = str_replace( [ '#', '$', '%', '^', '&', '*', '(', ')', '.', '=', '+'] , null, trim( $slug ) );
 		if(!$is_dash){
 			$slug = str_replace(['_','-'],null,trim($slug));
 		}
 		if($is_lang){
-			$slug = preg_replace('/[^a-z-A-Z\d\s!@#$%^&*\(\)_\-+=.]+/u',null,$slug);
+			$slug = preg_replace('/[^a-z-A-Z\d\s!@#$%^&*\(\)_\-+=.]+/u', null, $slug);
 		}
 		$slug = str_replace(' ',$concat,trim($slug));
 		return $slug;
